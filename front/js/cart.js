1cc503b4ee81ec1617e640026bf8160629972f80
soyyyyo <94390996+soyyyyo@users.noreply.github.com>
2022-05-13 10:43:37 +0200

    division des fichiers
    fichier precart réutilisable

---
+++ front/js/cart.js
@@ -0,0 +1,85 @@
+fetch("http://localhost:3000/api/products")
+  .then((rawData) => rawData.json()) // converti les data pour être lus
+  .then((okData) => {
+    // displayProducts(okData); // appel la fonction d'affichage du produit de la page
+    detailsOfCart(okData);
+    displayCart()
+    // console.table(okData); // affiche les data dans la console sous forme de tableau
+  })
+  .catch((err) => {
+document.querySelector("#cart__items").innerHTML += "<h1>erreur 404</h1>";
+console.log("erreur 404 via API: " + err); // définition de l'erreur dans la console
+});
+
+
+// définition du point d'entrée dans le HTML
+const toCartItem = document.querySelector("#cart__items");
+const toDeleteItem = document.querySelector(".deleteItem");
+
+/*
+Plan de bataille:
+- récupérer API
+- récupérer local storage => variable array
+- croiser les deux pour récupérer total data
+- insérer les data dans le html
+- event listener sur quantity
+- event listener sur supprimer
+- mise à jour du total
+
+
+*/
+
+
+// on récupére les détails de chaque produit dans une variable finalCart
+function detailsOfCart(data) {
+let localStorageCart = localStorage.getItem("cart"); // get cart de local storage
+finalCart = JSON.parse(localStorageCart)
+finalCart.forEach(product => {
+    const foundProduct = data.find(p => p._id === product.id);
+    if(foundProduct) {
+        product.name = foundProduct.name;
+        product.price = foundProduct.price;
+        product.imageUrl = foundProduct.imageUrl;
+        product.description = foundProduct.description;
+        product.altTxt = foundProduct.altTxt;
+    } else {
+        console.log("nothing was founded");
+    }
+});
+    console.log("final cart", finalCart);
+    return finalCart;
+}
+
+function displayCart() {
+    finalCart.forEach(product => {
+        toCartItem.innerHTML += 
+        `<article class="cart__item" data-id=${product._id} data-color=${product.cartColor}>
+    <div class="cart__item__img">
+    <img src=${product.imageUrl} alt=${product.altTxt}>
+    </div>
+    <div class="cart__item__content">
+    <div class="cart__item__content__description">
+        <h2>${product.name}</h2>
+        <p>${product.cartColor}</p>
+        <p>${product.price}</p>
+    </div>
+    <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+        <p>Qté : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${product.quantity}>
+        </div>
+        <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+        </div>
+    </div>
+    </div>
+</article>`
+    });
+}
+
+function deleteItem() {
+document
+    toDeleteItem.addEventListener("click", function(e){
+        console.log("caca boudin");
+    })
+}
--- front/js/product.js
+++ front/js/product.js
@@ -48,7 +48,7 @@ displayProducts = (products) => { // affichage du produit en fonction de son ID
 }
 
 // supprimer le local storage
-localStorage.clear();
+// localStorage.clear();
 // valable uniquement pendant les phases de test
 
 
@@ -57,80 +57,15 @@ localStorage.clear();
 // basket.addBasket({id:"25", price: 19, })
 // basket.removeFromBasket ==> renommer en "remove" etc
 
-
-class Cart {
-    constructor(){
-        let cart = localStorage.getItem("cart"); // get cart
-        if(cart == null) {
-            this.cart = [];
-        } else {
-            this.cart = JSON.parse(cart);
-        }
-    }
-
-    // sauvegarde le panier dans Local Storage
-    save() {
-        localStorage.setItem("cart", JSON.stringify(this.cart));
-    }
-
-    // ajoute un produit en vérifiant dans le local storage si son couple ID/Couleur existe déja
-    add(product) {
-        const foundProduct = this.cart.find(
-            p => p.id === product.id && p.cartColor === cartColor);
-        console.log("found product", foundProduct)
-
-        if(foundProduct){
-            // si on trouve l'ID && Couleur
-            foundProduct.quantity = parseInt(quantity) + parseInt(foundProduct.quantity);
-            console.log("j'existe déja");
-        } else {
-            // si l'ID ou Couleur est différent
-            product.quantity = quantity;
-            this.cart.push(product);
-            console.log("je suis nouveau")
-        }
-        this.save();
-    }
-
-
-    // supprime un produit, en filtrant tout ce qui n'est pas l'ID
-        remove(product){
-        this.cart = this.cart.filter(p => p.id != product.id);
-        this.save();
-    }
-    // change la quantité d'un produit
-        changeQuantity(product, quantity) {
-        let foundProduct = this.cart.find(p => p.id == product.id);
-        if(foundProduct != undefined) {
-            foundProduct.quantity += quantity
-            if(foundProduct.quantity <= 0) {
-                this.remove(foundProduct);
-            } else {
-            this.save();
-        }
-        }
-    }
-        getNumberProduct(){
-        let number = 0;
-        for(let product of this.cart){
-            number += product.quantity;
-        } 
-        return number;
-    }
-        getTotalPrice(){
-        let total = 0;
-        for(let product of this.cart){
-            total += product.quantity * product.price;
-        } 
-        return number;
-    }
-}
+////// le precart allait ici
 
 // défini les variables qui seront ajoutés au panier
 let id = pageId;
 let cartColor = "";
 let quantity = 0;
 let cart = new Cart;
+
+// let cart = new Cart;
 
 // surveille les changements de valeur de la couleur du produit
 document
